352174198a8f9dc22ea919af540c1d625be0845b
Ivan Patel <54930689+patelivan@users.noreply.github.com>
2020-09-19 13:35:55 -0700

    Update Census Data Collection .R

--- Census Data Collection .R
+++ Census Data Collection .R
@@ -11,6 +11,14 @@ Sys.getenv("CENSUS_API_KEY")
 v16 <- load_variables(year = 2018, dataset = 'acs5', cache = TRUE)
 hh_income <- filter(v16, str_detect(label, fixed('median household income', 
                                                  ignore_case = T)))
+
+v16 %>% filter(str_detect(label, fixed('time', ignore_case = T))) %>% View()
+
+# B11011_004
+# B11011_005
+x <- get_acs(geography = 'tract', variables = c(aggregate_travel_time_to_work_minutes = 'B08013_001'),
+        state = "CA", survey = 'acs5', year = 2018, output = 'wide')
+
 # Getting the data from 2010 to 2018 
 ca_data <- map_df(2010:2018, function(x) {
   get_acs(geography = "tract", variables = c(median.housing.value = "B25077_001",
@@ -20,28 +28,42 @@ ca_data <- map_df(2010:2018, function(x) {
                                              total.renter.occupied.housing.units = 'B25012_010',
                                              median.age = 'B01002_001',
                                              median.number.rooms = 'B25021_002',
-                                             median.year.built = 'B25037_002'), 
+                                             median.year.built = 'B25037_002',
+                                             married_couple_family_1_unit_structures = 'B11011_004',
+                                             married_couple_family_2_or_more_unit_structures = 'B11011_005',
+                                             aggregate_travel_time_to_work_minutes = 'B08013_001'
+                                             ), 
           
-          state = "CA", survey = 'acs5', year = x, geometry = T, cache_table = T, output = 'wide') %>%
+          state = "CA", survey = 'acs5', year = x, geometry = T, output = 'wide') %>%
     mutate(year = x)
-})
+}) 
 
-# Write the data
+# Changing the column names
+names(ca_data)[3:18] <- c("median.housing.value.estimate", "median.housing.value.moe",
+    "median.household.income.estimate", "median.household.income.moe",
+    "population.estimate", "population.moe", "total.owner.occupied.housing.units.estimate",
+    "total.owner.occupied.housing.units.moe", 'total.renter.occupied.housing.units.estimate', 
+    'total.renter.occupied.housing.units.moe',
+    'median.age.estimate','median.age.moe' ,'median.number.rooms.estimate', 'median.number.rooms.moe',
+    'median.year.built.estimate', 'median.year.built.moe'
+    )
+
+# Write the data to a shape file for easy access
 write_sf(ca_data, 'ca_data.shp')
 
-# Changing the names
-names(ca_data)
-names(ca_data)[3:8] <- c("median.housing.value.estimate", "median.housing.value.moe",
-                         "median.household.income.estimate", "median.household.income.moe",
-                         "population.estimate", "population.moe")
+# md HSH E - median household income
+# md HSN E - median housing value
+# md n E - median.number.rooms.estimate
+# md yE - median Year Built
+# medn ge - median.age
+# popltn E - population
+# ttl__r__E - total.renter.occupied.housing.units.estimate
+# ttl__w___E - total.owner.occupied.housing.units
 
+# Glimpse the data and its Coordinate Reference System
+ca_data %>% glimpse()
+st_crs(ca_data) # North American Datum 1983. 
 
-# Code used to search for variables 
-v16 %>% filter(str_detect(label, fixed('Estimate!!Total!!Owner-occupied housing units', 
-                            ignore_case = T))) %>% View()
-v16 %>% filter(name == 'B01002_001')
-x <- get_acs(geography = "tract", variable = 'B25012_010',
-             state = "CA", survey = 'acs5', year = 2018, geometry = T, cache_table = T) 
 
 
 
